a16ac1329b8aa734d1aace82ad662bb9f7aa686a
Alejandro Phocco <aphoccot@unsa.edu.pe>
2023-06-11 18:26:59 -0500

    Se creo un form para el modelo Post y se cambiaron las paginas newPost que ahora muestra un formulario y puede ser enviado para crear un Post, editPost que muestra la informacion del Post y puede ser editado. Por ultimo se añadio la opcion de eliminar un post, adems se modificaron sus urls y views respectivas

--- PersonalBlog/Blog/urls.py
+++ PersonalBlog/Blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name = 'index'),
     path('new_post/', views.newPost, name = "new_post" ),
     path('post/<int:id>', views.post, name = "post"),
-    path('edit_post/<int:id>', views.editPost, name = "edit_post")
+    path('edit_post/<int:id>', views.editPost, name = "edit_post"),
+    path('delete_post/<int:id>', views.deletePost, name = "delete_post")
 ]
--- PersonalBlog/Blog/views.py
+++ PersonalBlog/Blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import CreateNewPost
 
 # Create your views here.
 
@@ -8,12 +9,28 @@ def index(request):
     return render(request, 'index.html', {'posts': posts})
 
 def newPost(request):
-    return render(request, 'newPost.html')
+    if request.method == 'GET':
+        return render(request, 'newPost.html', {'form': CreateNewPost()})
+    else:
+        Post.objects.create(title=request.POST['title'], text=request.POST['text'])
+        return redirect('/')
 
 def post(request, id):
     post = Post.objects.get(id=id)
     return render(request, 'post.html', {'post': post})
 
 def editPost(request, id):
+    if request.method == 'GET':
+        post = Post.objects.get(id=id)
+        form = CreateNewPost(instance=post)
+        return render(request, 'editPost.html', {'form' : form})
+    else:
+        post = Post.objects.get(id=id)
+        form = CreateNewPost(request.POST, instance=post)
+        form.save()
+        return redirect('/')
+
+def deletePost(request, id):
     post = Post.objects.get(id=id)
-    return render(request, 'editPost.html', {'post' : post})
+    post.delete()
+    return redirect('/')
